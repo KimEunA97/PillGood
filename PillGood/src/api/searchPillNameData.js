f0ada24f0607ceaf863dc775134f86a3d1128e18
KimEunA97 <kimeuna9712@gmail.com>
2023-10-02 06:59:34 +0900

    비동기 fetch. then 대신 await으로 체이닝을 사용하지 않고 JSON 데이터  처리

--- PillGood/src/api/searchPillNameData.js
+++ PillGood/src/api/searchPillNameData.js
@@ -4,16 +4,39 @@ dotenv.config();
 
 const API_KEY = process.env.API_KEY;
 
+
 const URL = `https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=`
-const _URL = URL + API_KEY + `&type=xml`;
+const _URL = URL + API_KEY + `&type=json`;
 
 function searchPillNameData(pillName) {
 
+
   const isName = `&itemName=`
   const data = _URL + isName + pillName;
-  console.log(_URL + isName + pillName);
+  // console.log(data)
   return data;
 
 }
 
-searchPillNameData(name);
+async function fetchedPillNameData() {
+
+  const name = "타이레놀"
+  const test = searchPillNameData(name)
+
+  try {
+    const res = await fetch(test);
+    const data = await res.json();
+    console.dir(data.body.totalCount)
+
+    // await fetch(test)
+    //   .then((res) => { res.json() })
+    //   .then((data) => console.dir(data))
+
+  }
+  catch (error) {
+    console.error();
+  }
+
+}
+
+fetchedPillNameData()
