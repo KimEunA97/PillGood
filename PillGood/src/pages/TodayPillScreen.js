22332c1c340df5c50da38ff29c4677689b04996b
KimEunA97 <kimeuna9712@gmail.com>
2023-10-04 05:01:39 +0900

    create 버튼 스타일링

--- PillGood/src/pages/TodayPillScreen.js
+++ PillGood/src/pages/TodayPillScreen.js
@@ -1,16 +1,36 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Pressable } from "react-native";
 
 export default function TodayPillScreen() {
 
   return (
-    <View>
-      <Text>오늘의약</Text>
+    <View style={styles.container}>
+      <Pressable style={styles.box}>
+        <Text style={styles.text}>Press it. img section</Text>
+      </Pressable>
     </View>
   )
 
 }
 
 const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "center",
 
+  },
+  box: {
+    width : "60vw",
+    height : "15vh",
+    borderWidth: 1,
+    borderColor: "white",
+    justifyContent : "center",
+    alignItems : "center"
+  },
+  text: {
+    color: "white",
+
+  }
 })
