ecd06f4e959aad3e0213274bb179974c83002618
KimEunA97 <kimeuna9712@gmail.com>
2023-10-03 22:04:11 +0900

    페이지 컴포넌트 네개 생성

---
+++ PillGood/src/pages/TodayPillScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+export default function TodayPillScreen() {
+
+  return (
+    <View>
+
+    </View>
+  )
+
+}
+
+const styles = StyleSheet.create({
+
+})
