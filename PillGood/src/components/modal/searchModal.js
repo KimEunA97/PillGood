88a5050f662dec837152d362d741b18fe79bce56
KimEunA97 <kimeuna9712@gmail.com>
2023-10-05 05:48:31 +0900

    외부 클릭시 닫는 기능 나중에 추가하기

--- PillGood/src/components/modal/searchModal.js
+++ PillGood/src/components/modal/searchModal.js
@@ -1,32 +1,41 @@
 import React, { useState } from "react";
-import { Modal, Text, StyleSheet, View, TouchableWithoutFeedback, Pressable } from "react-native";
+import { Modal, Text, StyleSheet, View, TouchableWithoutFeedback, Pressable, TextInput } from "react-native";
 
 function SearchModal({ isVisible, onClose }) {
 
+  const [text, onChangeText] = useState('');
+
+  //내부를 눌러도 닫히지 않는 함수
+
   return (
-    <Pressable onPress={onClose}>
-      <Modal
-        transparent={true}
-        presentationStyle={"fullScreen"}
-        visible={isVisible}
-        animationType="fade"
 
+    <Modal
+      transparent={true}
+      presentationStyle={"fullScreen"}
+      visible={isVisible}
+      animationType="fade"
+    >
+
+      <Pressable
+        style={styles.container}
       >
-        <View style={styles.container}>
-          <View style={styles.content}>
-            <Text>asdasdasd</Text>
+        <View style={styles.content}>
 
-            <View style={{ flexDirection: "row", margin : 10, }}>
-              <Pressable onPress={onClose} style={{ width: 100, height: 100, backgroundColor: "red" }} />
-              <Pressable onPress={onClose} style={{ width: 100, height: 100, backgroundColor: "blue" }} />
-            </View>
-
+          <Text style={styles.textStyle}>asdasdasd</Text>
+          <TextInput
+            style={styles.textInputStyle}
+            onChangeText={onChangeText}
+            value={text}
+          />
+          {/* 상호작용 버튼 */}
+          <View style={{ flexDirection: "row", margin: 10, }}>
+            <Pressable onPress={onClose} style={{ width: "50%", height: "30%", backgroundColor: "red" }} />
+            <Pressable onPress={onClose} style={{ width: 100, height: 100, backgroundColor: "blue" }} />
           </View>
+
         </View>
-
-
-      </Modal>
-    </Pressable>
+      </Pressable>
+    </Modal >
   )
 }
 
@@ -38,19 +47,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "rgba(0,0,0,0.5)"
-
+    backgroundColor: "rgba(0,0,0,0.5)",
   },
   content: {
     justifyContent: "center",
     alignItems: "center",
-
     width: "50%",
     height: "70%",
     backgroundColor: "#007088",
-
     borderRadius: 10,
 
+  },
+  textInputStyle: {
+    backgroundColor: "white"
+  },
+  textStyle: {
+    fontSize: 50,
+    color: "white",
   }
 
 })
