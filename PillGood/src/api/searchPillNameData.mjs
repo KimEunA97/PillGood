bc2260a3899072b0cb288397e3d59f16398d9c86
KimEunA97 <kimeuna9712@gmail.com>
2023-10-08 23:10:58 +0900

    commonjs 충돌. mjs 확장자 변경으로 해결?

--- PillGood/src/api/searchPillNameData.mjs
+++ PillGood/src/api/searchPillNameData.mjs
@@ -1,32 +1,23 @@
-const dotenv = require('dotenv')
+import dotenv from 'dotenv'
 
 dotenv.config();
 
-const API_KEY = process.env.API_KEY;
-
-
-const URL = `https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=`
-const _URL = URL + API_KEY + `&type=json`;
-
 async function searchPillNameData(pillName) {
 
+  const API_KEY = process.env.API_KEY;
+  const URL = `https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=`
+  const _URL = URL + API_KEY + `&type=json`;
   const isName = `&itemName=`
   const reqURL = _URL + isName + pillName;
   // console.log(reqURL)
-
   try {
     const res = await fetch(reqURL);
     const data = await res.json();
     // console.dir(data.body)
-
     return data;
-
   }
-
   catch (error) {
     console.error();
   }
-
 }
 
-module.exports = searchPillNameData;
