88ef1e782c56ace97bcc79edc64c9a4f7a2f72c1
KimEunA97 <kimeuna9712@gmail.com>
2023-10-08 11:40:09 +0900

    아니다. 배열 문제 해결 안됐음..

--- PillGood/src/components/PillList.js
+++ PillGood/src/components/PillList.js
@@ -3,10 +3,9 @@ import { View, Text } from "react-native";
 
 const PillList = ({ items }) => {
 
-  const itemList = Array.isArray(items) ? items : [];
   return (
     <View>
-      {itemList.map((item, index) => (
+      {items.map((item, index) => (
         <Text key={index}>{item}</Text>
       ))}
     </View>
