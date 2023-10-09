12c5c2a510c40d03c54f8dd441b66df67e65a63e
KimEunA97 <kimeuna9712@gmail.com>
2023-10-09 11:27:02 +0900

    totalCount 위치 변경, 배열 변수에 model 구조 직접 알려줘서 전달

--- PillGood/src/model/pillModel.js
+++ PillGood/src/model/pillModel.js
@@ -1,12 +1,12 @@
 //데이터 모델
 function pillModel(data) {
   const result = {
+    //데이터 갯수
+    totalCount: data.body.totalCount,
     //이름
     name: data.body.items.item.itemName,
     //증상
     Symptom: data.body.items.item.efcyQesitm,
-    //데이터 갯수
-    totalCount: data.body.items.item.totalCount,
     //사용법
     useage: data.body.items.item.USE_METHOD_QESITM,
     //주의사항
@@ -18,21 +18,34 @@ function pillModel(data) {
 // 임의의 API 응답 데이터를 생성합니다.
 const apiResponseData = {
   body: {
+    totalCount : 3,
     items: {
-      item: {
-        itemName: "테스트 약 이름",
-        efcyQesitm: "테스트 증상",
-        totalCount: 42,
-        USE_METHOD_QESITM: "테스트 사용법",
-        ATPN_WARN_QESITM: "테스트 주의사항"
-      }
+      item: [
+        {
+          itemName: "테스트 약 이름 1",
+          efcyQesitm: "테스트 증상 1",
+          USE_METHOD_QESITM: "테스트 사용법 1",
+          ATPN_WARN_QESITM: "테스트 주의사항 1"
+        },
+        {
+          itemName: "테스트 약 이름 2",
+          efcyQesitm: "테스트 증상 2",
+          USE_METHOD_QESITM: "테스트 사용법 2",
+          ATPN_WARN_QESITM: "테스트 주의사항 2"
+        },
+        {
+          itemName: "테스트 약 이름 3",
+          efcyQesitm: "테스트 증상 3",
+          USE_METHOD_QESITM: "테스트 사용법 3",
+          ATPN_WARN_QESITM: "테스트 주의사항 3"
+        }
+      ]
     }
   }
 };
 
-// 데이터 모델 함수를 호출하여 데이터를 추출합니다.
-let arr = [];
-const result = pillModel(apiResponseData);
-arr.push(result);
-// 결과를 출력해 봅니다.
-console.log(arr, "arr")
+const items = apiResponseData.body.items.item;
+const arr = items.map((item) => pillModel({ body: { items: { item } } }))
+
+// console.dir(arr, "arr");
+console.log(arr[2].name, "arr2")
