2b2f93bf2535ee8b8d6e76e99df9d41b40862145
KimEunA97 <kimeuna9712@gmail.com>
2023-10-25 09:44:43 +0900

    hidding key

--- PillGood/src/api/getPillNameData.js
+++ PillGood/src/api/getPillNameData.js
@@ -2,7 +2,6 @@ import Constants from 'expo-constants';
 
 async function getPillNameData(pillName) {
   // const API_KEY = Constants.manifest2.extra.API_KEY;
-  const API_KEY = `1Tr4rV6KC3cceg8vKqCsXmw%2ByfY51F96gYNV9usKql8PWixtz4ssFfo%2FgjASLni1zcLMEt%2FHdDIZlOzc3QBfuw%3D%3D`
   console.log(API_KEY, "API_KEY");
   const URL = `https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=`
   const _URL = URL + API_KEY + `&type=json` + `&numOfRows=50`;
