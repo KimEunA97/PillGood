51ed5a87e9759e69f60baac379b9fee891c83812
KimEunA97 <kimeuna9712@gmail.com>
2023-10-25 11:55:38 +0900

    검색모달창 스타일 조정

--- PillGood/src/components/modal/searchModal.js
+++ PillGood/src/components/modal/searchModal.js
@@ -19,25 +19,29 @@ function SearchModal({ isVisible, onClose, confirm }) {
     >
 
       <Pressable
-        style={styles.container}
-      >
+        style={styles.container}>
         <View style={styles.content}>
+            <Text style={styles.textStyle}>약이름</Text>
+            <TextInput
+              style={styles.textInputStyle}
+              onChangeText={onChangeText}
+              placeholder="약이름"
+              placeholderTextColor={"grey"}
+              value={text}/>
 
-          <Text style={styles.textStyle}>약이름</Text>
-          <TextInput
-            style={styles.textInputStyle}
-            onChangeText={onChangeText}
-            placeholder="약이름"
-            placeholderTextColor={"grey"}
-            value={text}
-          />
-
-          {/* 상호작용 버튼 */}
-          <View style={{ width: 200, height: 100, flexDirection: "row", margin: 10, }}>
-            <Pressable onPress={handleConfirm} style={{ width: "50%", height: "30%", backgroundColor: "red" }} />
-            <Pressable onPress={onClose} style={{ width: "50%", height: "30%", backgroundColor: "blue" }} />
+          <View>
+            <Text style={styles.textStyle}>시간설정</Text>
           </View>
 
+          {/* 상호작용 버튼 */}
+          <View style={styles.buttonContainer}>
+            <Pressable onPress={handleConfirm} style={[styles.buttonSty, { backgroundColor: "blue" }]}>
+              <Text style={styles.buttonTextSty}>확인</Text>
+            </Pressable>
+            <Pressable onPress={onClose} style={[styles.buttonSty, { backgroundColor: "red" }]}>
+              <Text style={styles.buttonTextSty}>취소</Text>
+            </Pressable>
+          </View>
         </View>
       </Pressable>
     </Modal >
@@ -55,22 +59,41 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(0,0,0,0.5)",
   },
   content: {
-    justifyContent: "center",
+    flexDirection: "column",
+    justifyContent : "center",
     alignItems: "center",
-    width: "50%",
+    width: "70%",
     height: "70%",
     backgroundColor: "#007088",
     borderRadius: 10,
-
   },
   textInputStyle: {
     width: "80%",
     height: "5%",
-    backgroundColor: "white"
+    backgroundColor: "white",
+    marginTop : 10,
   },
   textStyle: {
-    fontSize: 50,
+    fontSize: 40,
     color: "white",
+  },
+  buttonContainer: {
+    width: "80%",
+    height: "20%",
+    flexDirection: "row",
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  buttonSty: {
+    width: "45%",
+    height: "50%",
+    borderRadius: 10,
+  },
+  buttonTextSty: {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 30,
   }
-
 })
