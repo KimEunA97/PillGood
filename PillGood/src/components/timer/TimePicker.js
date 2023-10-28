df8a823f6c779ccf64f89bd1a80bea5e2fae5609
KimEunA97 <kimeuna9712@gmail.com>
2023-10-28 16:46:16 +0900

    시간 일치하는 걸 어떻게 핸들링 해서 전달할지

--- PillGood/src/components/timer/TimePicker.js
+++ PillGood/src/components/timer/TimePicker.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
-import { View, Button, Text, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Button, Text, Platform, StyleSheet } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function TimePicker() {
   // 1초 * 60 = 60초(1분) * 60 = 60분 * 9 = 9시간
   const UTC9 = 1000 * 60 * 60 * 9  //한국시간 UTC+9
   const [time, setTime] = useState(new Date((new Date()).getTime() + UTC9));
-  console.log(time, "현재시간")
+  const [selectedTime, setSelectedTime] = useState(0);
   const [show, setShow] = useState(false);
+  const [timeSetToggle, setTimeSetToggle] = useState(false)
 
   const onChange = (event, selectedTime) => {
+    console.log(time, "한국시간")
     if (selectedTime) {
-      setShow(Platform.OS === 'ios'); // iOS에서는 시간을 선택하면 자동으로 모달이 닫히므로 Android에서만 모달을 제어합니다.
+      setShow(Platform.OS === 'ios');
+      setSelectedTime(selectedTime);
       setTime(selectedTime);
-      console.log(show)
+      console.log(selectedTime, "선택된 시간");
+      timeSetDone(); // 시간설정완료
+      pushAlarm();
     }
   };
+
+  const timeSetDone = () => {
+    setTimeSetToggle(true);
+  }
+
+  const pushAlarm = () => {
+
+    // let leftTime = time - selectedTime
+    // console.log(leftTime, "시간계산");
+
+  }
 
   return (
     <View>
@@ -25,11 +41,29 @@ export default function TimePicker() {
           locale='KO'
           value={time}
           mode="time"
-          is24Hour={true}
+          is24Hour={false}
           onChange={onChange}
+          on
+          display='spinner'
         />
       )}
-      <Text>Selected Time: {time.toLocaleTimeString()}</Text>
-    </View>
+      {/* <Text style={styles.textSty}>알림 시간</Text> */}
+      {timeSetToggle ?
+        <Text style={styles.timeTextStyle}> 설정시간 : {selectedTime.toLocaleTimeString('ko-KR')}</Text>
+        :
+        <Text style={styles.timeTextStyle}> 현재시간 : {time.toLocaleTimeString('ko-KR')}</Text>}
+    </View >
   );
 }
+
+const styles = StyleSheet.create({
+  textSty: {
+    color: "white",
+    fontSize: 30,
+  },
+  timeTextStyle: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
+})
