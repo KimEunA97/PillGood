81a6548e99645e7e99c8d5b71f03cbb2303f06ec
KimEunA97 <kimeuna9712@gmail.com>
2024-05-19 20:18:52 +0900

    컴포넌트 연결

--- app-PillGood/App.js
+++ app-PillGood/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, TouchableOpacity, Text, SafeAreaView, StyleSheet } from 'react-native';
+
+import NavigationBar from './src/components/nav/NavigationBar';
+
+import PillDetailScreen from './src/pages/PillDetailScreen';
+import SearchPillScreen from './src/pages/SearchPillScreen';
+import TodayPillScreen from './src/pages/TodayPillScreen';
+import UserSettingScreen from './src/pages/UserSettingScreen';
 
 export default function App() {
+
+  const [currentPage, setCurrentPage] = useState('TodayPillScreen')
+  const handleNavigation = (page) => {
+    setCurrentPage(page);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <NavigationBar onPageChange={handleNavigation} currentPage={currentPage} />
+        {currentPage === "TodayPillScreen" && <TodayPillScreen />}
+        {currentPage === "SearchPillScreen" && <SearchPillScreen />}
+        {currentPage === "PillDetailScreen" && <PillDetailScreen />}
+        {currentPage === "UserSettingScreen" && <UserSettingScreen />}
+      </View>
+    </SafeAreaView >
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "grey"
+  }
+})
