558af8953919d64573c2b423e94cec229d693c90
KimEunA97 <kimeuna9712@gmail.com>
2024-10-03 17:37:07 +0900

    모달이 떴써염

--- nb-pillgood/components/PillModal.tsx
+++ nb-pillgood/components/PillModal.tsx
@@ -1,4 +1,14 @@
-import { Box, Button, Input, Modal } from "native-base";
+import {
+  Box,
+  Button,
+  FormControl,
+  HStack,
+  Input,
+  Modal,
+  Text,
+  VStack,
+  WarningOutlineIcon,
+} from "native-base";
 import { useState } from "react";
 
 interface PillModalProps {
@@ -9,16 +19,26 @@ interface PillModalProps {
 export default function PillModal({ visible, onClose }: PillModalProps) {
   return (
     <Modal isOpen={visible} onClose={onClose}>
-      <Modal.Content maxWidth="400px" height="600px">
+      <Modal.Content maxWidth="400px" height="600px" bgColor="cyan.600">
         <Modal.CloseButton />
         <Modal.Header>등록할 약 검색하기</Modal.Header>
         <Modal.Body>
-          <Box alignItems="center">
-            <Input mx="3" placeholder="Input" w="100%" />
-          </Box>
+          <VStack space={3} alignItems={"center"}>
+            <Text color="white" fontSize="lg" mb={1}>
+              약 이름
+            </Text>
+            <Input mx="3" placeholder="약 이름" w="90%" bgColor="light.100" />
+            <Text color="white" fontSize="lg" mb={1}>
+              제조사명
+            </Text>
+            <Input placeholder="그려" bgColor="light.100" mx="3" w="90%" />
+          </VStack>
         </Modal.Body>
         <Modal.Footer>
-          <Button onPress={onClose}>Close</Button>
+          <HStack flex={1} justifyContent="space-between">
+            <Button onPress={() => console.log("등록")}>등록</Button>
+            <Button onPress={onClose}>Close</Button>
+          </HStack>
         </Modal.Footer>
       </Modal.Content>
     </Modal>
