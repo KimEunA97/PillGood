0522c6bfc85d7e8a763ac5a9c5aee86666b15136
KimEunA97 <kimeuna9712@gmail.com>
2024-11-01 03:06:05 +0900

    global.css 파일 import 불가 현상

--- paper-pillgood/pages/PillCalendar.tsx
+++
@@ -1,3 +0,0 @@
-export default function PillCalendar() {
-  return <div></div>;
-}
---
+++ rework-pillgood/App.tsx
@@ -0,0 +1,24 @@
+import { GluestackUIProvider } from "components/ui/gluestack-ui-provider";
+import { StatusBar } from "expo-status-bar";
+
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <GluestackUIProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </GluestackUIProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
