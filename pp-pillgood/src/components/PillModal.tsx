f68122c21fe2df113da20dc3a359070e8d117276
KimEunA97 <kimeuna9712@gmail.com>
2024-11-13 09:38:28 +0900

    약 이름 검색 절차 구현 완료

--- pp-pillgood/src/components/PillModal.tsx
+++ pp-pillgood/src/components/PillModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Appbar,
   Button,
@@ -19,16 +19,49 @@ interface PillModalProps {
   closeModal: () => void; // 함수 타입 정의
 }
 
+interface Pill {}
+
 export default function PillModal({ visible, closeModal }: PillModalProps) {
   const [searchPillName, setSearchPillName] = useState("");
   const [currentStep, setCurrentStep] = useState(1);
+  const [pillData, setPillData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("../data.json"); // 실제 파일 경로
+        const data = await response.json();
+        console.log(data.body.items);
+        setPillData(data.body.items);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // 검색하기 버튼 이벤트
+  const searchingPillData = () => {
+    setLoading(true);
+    setCurrentStep(2);
+  };
+  const handleSelectedData = (pillName: string) => {
+    setSearchPillName(pillName);
+    setCurrentStep(1);
+  };
+
+  // 다음으로 버튼 이벤트
   const nextButton = () => {
-    if (currentStep !== 2) {
+    // 절차.1 검색한다.
+    // 절차.2 선택한다.
+    // 절차.3 등록한다.
+    if (currentStep !== 3) {
       setCurrentStep((prev) => prev + 1);
     }
     console.log(currentStep);
   };
+  // 모달 닫기
   const handleCloseModal = () => {
     setCurrentStep(1);
     closeModal();
@@ -45,22 +78,28 @@ export default function PillModal({ visible, closeModal }: PillModalProps) {
           <View style={{ height: "90%" }}>
             <Appbar.Header>
               <Appbar.BackAction onPress={handleCloseModal} />
-              <Appbar.Content title="약 등록하기" />
+              <Appbar.Content title="약 검색하기" />
             </Appbar.Header>
             <View style={styles.modalContent}>
-              <Text variant="labelLarge">약 이름</Text>
-              <TextInput
-                label="약 이름"
-                value={searchPillName}
-                onChangeText={(text) => setSearchPillName(text)}
-                onSubmitEditing={() => console.log("key")}
-              />
-
-              <TextInput
-                label="약 이름"
-                value={searchPillName}
-                onChangeText={(text) => setSearchPillName(text)}
-              ></TextInput>
+              <Text
+                variant="labelLarge"
+                style={{ textAlign: "center", width: "70%" }}
+              >
+                약을 검색하거나 직접 등록하세요!
+              </Text>
+              <View>
+                <TextInput
+                  label="약 이름"
+                  value={searchPillName}
+                  onChangeText={(text) => setSearchPillName(text)}
+                  onSubmitEditing={() => console.log("key")}
+                  style={{ marginBottom: 10 }}
+                />
+                <DefaultButton
+                  text="검색하기"
+                  onPress={searchingPillData}
+                ></DefaultButton>
+              </View>
             </View>
           </View>
         )}
@@ -72,8 +111,24 @@ export default function PillModal({ visible, closeModal }: PillModalProps) {
               <Appbar.Content title="약 선택하기" />
             </Appbar.Header>
             <View style={styles.modalContent}>
-              <Text variant="labelLarge">약 이름</Text>
+              {pillData.map((item, index) => (
+                <View key={index}>
+                  <DefaultButton
+                    text={item.itemName}
+                    onPress={() => handleSelectedData(item.itemName)}
+                  ></DefaultButton>
+                </View>
+              ))}
             </View>
+          </View>
+        )}
+
+        {currentStep === 3 && (
+          <View>
+            <Appbar.Header>
+              <Appbar.BackAction onPress={handleCloseModal} />
+              <Appbar.Content title="약 등록하기" />
+            </Appbar.Header>
           </View>
         )}
 
