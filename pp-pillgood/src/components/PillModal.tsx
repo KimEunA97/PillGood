42547a2c5f9bbbd7246592c4282c82f35ba62a91
KimEunA97 <kimeuna9712@gmail.com>
2024-11-13 09:53:39 +0900

    버튼 텍스트 볼드 제거, 검색 후 나타나는 약 선택 리스트 버튼들이 가운데로 정렬 안되는 현상

--- pp-pillgood/src/components/PillModal.tsx
+++ pp-pillgood/src/components/PillModal.tsx
@@ -10,7 +10,7 @@ import {
   TextInput,
   useTheme,
 } from "react-native-paper";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import DefaultButton from "./DefaultButton";
 import PillSearchModal from "./PillSearchModal";
 
@@ -30,7 +30,7 @@ export default function PillModal({ visible, closeModal }: PillModalProps) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("../data.json"); // 실제 파일 경로
+        const response = await fetch("../data2.json"); // 실제 파일 경로
         const data = await response.json();
         console.log(data.body.items);
         setPillData(data.body.items);
@@ -59,25 +59,31 @@ export default function PillModal({ visible, closeModal }: PillModalProps) {
     setCurrentStep(3);
     console.log(currentStep);
   };
-  // 모달 닫기
-  const handleCloseModal = () => {
+
+  // 뒤로가기
+  const prevButton = () => {
     setCurrentStep((prev) => prev - 1);
     if (currentStep === 1) {
       closeModal();
     }
   };
 
+  const handleCloseModal = () => {
+    closeModal();
+    setCurrentStep(1);
+  };
+
   return (
     <Portal>
       <Modal
         visible={visible}
-        onDismiss={handleCloseModal}
+        onDismiss={prevButton}
         contentContainerStyle={styles.modalContainer}
       >
         {currentStep === 1 && (
           <View style={{ height: "90%" }}>
             <Appbar.Header>
-              <Appbar.BackAction onPress={handleCloseModal} />
+              <Appbar.BackAction onPress={prevButton} />
               <Appbar.Content title="약 검색하기" />
             </Appbar.Header>
             <View style={styles.modalContent}>
@@ -107,18 +113,29 @@ export default function PillModal({ visible, closeModal }: PillModalProps) {
         {currentStep === 2 && (
           <View style={{ height: "90%" }}>
             <Appbar.Header>
-              <Appbar.BackAction onPress={handleCloseModal} />
+              <Appbar.BackAction onPress={prevButton} />
               <Appbar.Content title="약 선택하기" />
             </Appbar.Header>
             <View style={styles.modalContent}>
-              {pillData.map((item, index) => (
-                <View key={index}>
-                  <DefaultButton
-                    text={item.itemName}
-                    onPress={() => handleSelectedData(item.itemName)}
-                  ></DefaultButton>
+              <ScrollView>
+                <View>
+                  {pillData.map((item, index) => (
+                    <View
+                      style={{
+                        marginBottom: 10,
+                        width: "70%",
+                        justifyContent: "center",
+                      }}
+                      key={index}
+                    >
+                      <DefaultButton
+                        text={item.itemName}
+                        onPress={() => handleSelectedData(item.itemName)}
+                      ></DefaultButton>
+                    </View>
+                  ))}
                 </View>
-              ))}
+              </ScrollView>
             </View>
           </View>
         )}
@@ -126,8 +143,8 @@ export default function PillModal({ visible, closeModal }: PillModalProps) {
         {currentStep === 3 && (
           <View>
             <Appbar.Header>
-              <Appbar.BackAction onPress={handleCloseModal} />
-              <Appbar.Content title="약 등록하기" />
+              <Appbar.BackAction onPress={prevButton} />
+              <Appbar.Content title="알림 설정 " />
             </Appbar.Header>
           </View>
         )}
