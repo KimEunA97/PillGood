389b02e3ff1f78633f666fbf41b810120187d374
KimEunA97 <kimeuna9712@gmail.com>
2024-11-14 20:17:27 +0900

    타임피커 설정 + 바벨 빌드 설정 변경

--- pp-pillgood/babel.config.js
+++ pp-pillgood/babel.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: ["babel-preset-expo"], // Expo의 기본 Babel 프리셋 사용
   env: {
     production: {
-      plugins: ["react-native-paper/babel"],
+      plugins: ["react-native-paper/babel"], // react-native-paper의 babel 플러그인 설정
     },
   },
 };
